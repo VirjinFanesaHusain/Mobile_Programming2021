829f7d8622bfcb2d7f38f3a253f19643a8a84698
VirjinFanesaHusain <S21910116@student.unklab.ac.id>
2021-03-10 05:07:40 +0800

    Array & Object

---
+++ Pertemuan_04/index.js
@@ -0,0 +1,23 @@
+//Array
+
+// let number = [1,2,3,4,5];
+// let virgin = ["virgin", "Husain", 20, true];
+
+// let number = new Array(1,2,3,4,5);
+// let virgin = new Array("Virgin", "Husain", 20, true);
+
+// virgin[3] = false;
+
+// console.log(numbers[numbers.length - 1]);
+// console.log(virgin[virgin.length - 1]);
+
+//Object
+const virgin = {
+    firstName: "Virgin",
+    lastName: "Fanesa",
+    age: 20,
+    isMarried: false,
+};
+
+console.log(virgin.lastName);
+console.log(virgin["isMarried"])
