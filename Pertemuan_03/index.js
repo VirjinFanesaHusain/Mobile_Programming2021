75093100742b41616dc32b8720c75f176c110fe4
VirjinFanesaHusain <S21910116@student.unklab.ac.id>
2021-03-10 04:58:14 +0800

    Variabel,Operator,Function

---
+++ Pertemuan_03/index.js
@@ -0,0 +1,31 @@
+let hello;
+hello = "Hello Virjin";
+
+const birthYear = 2001;
+
+let age = 20;
+age += 2;
+let apakahSudahMenikah = true;
+let alamat;
+
+console.log(hello);
+console.log(birthYear);
+console.log(age);
+console.log(apakahSudahMenikah);
+console.log(alamat);
+
+let namaDepan = "Virjin";
+let namaBelakang = "Fanesa";
+
+let namaLengkap = namaDepan + " " + namaBelakang + ",Usia saya adalah 20 tahun" + age;
+
+console.log(namaLengkap);
+
+console.log(10 == "10");
+console.log(10 === "10");
+
+//Function declaration
+const ucapkanSalam = function(){
+    console.log("Selamat siang dari Virjin");
+}
+ucapkanSalam();
