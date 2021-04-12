e7493df9d9362b8c95cc9dbd9bdf0b225c148986
VirjinFanesaHusain <S21910116@student.unklab.ac.id>
2021-04-12 23:33:25 +0800

    Ubah nama Login jadi Register

--- AwesomeProject/src/App.js
+++ AwesomeProject/src/App.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import Login from './Components/pages/Login';
+import Register from './Components/pages/Register';
 import UserList from './Components/pages/UserList';
 
 const App = () =>{
