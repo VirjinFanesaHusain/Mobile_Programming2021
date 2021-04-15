82555cde6bdae064201c104ab20c79f9262f8f05
VirjinFanesaHusain <S21910116@student.unklab.ac.id>
2021-04-15 20:38:56 +0800

    Json file-test

--- AwesomeProject/src/App.js
+++ AwesomeProject/src/App.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import Register from './Components/pages/Register';
 import UserList from './Components/pages/UserList';
+import db from './db.json';
 
 const App = () =>{
     return <UserList />;
