56a42f93d157006b0664e8350a4c3cfcb3dcd7cd
VirjinFanesaHusain <S21910116@student.unklab.ac.id>
2021-04-18 22:13:42 +0800

    Update

--- AwesomeProject/src/App.js
+++ AwesomeProject/src/App.js
@@ -1,10 +1,10 @@
 import React from 'react';
 import Register from './Components/pages/Register';
 import UserList from './Components/pages/UserList';
-
+import Login from './Components/pages/Login'
 
 const App = () =>{
-    return <UserList />;
+    return <Login />;
 };
 
 export default App;
